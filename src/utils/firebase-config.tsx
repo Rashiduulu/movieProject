a94a7ba80f39f916d6255c6885af0f1a9d375c5b
Nurtilek Rashid uulu <98835365+Rashiduulu@users.noreply.github.com>
2024-01-04 22:06:27 +0900

    Update firebase-config.tsx

--- src/utils/firebase-config.tsx
+++ src/utils/firebase-config.tsx
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { Firestore, getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCxGFDGv626YM3Z8ZqBqexSijBEZkTxcM",
-  authDomain: "movie-project-90e71.firebaseapp.com",
-  projectId: "movie-project-90e71",
-  storageBucket: "movie-project-90e71.appspot.com",
-  messagingSenderId: "928281739974",
-  appId: "1:928281739974:web:1f33213fa0cca9603e8dc7",
-  measurementId: "G-56VSBN2MRD",
+  apiKey: "",
+  authDomain: "",
+  projectId: "movie-project",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
